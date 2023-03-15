294b5f0cbbef5a0a64983fb67d2657507e73b497
gregory hendrickson <greg@hndrx.co>
2023-03-15 16:03:16 -0700

    remove dupes

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -137,46 +137,7 @@ resource "google_compute_global_forwarding_rule" "fancy_http_rule" {
 }
 
 
-#Create HealthChecks
 
-resource "google_compute_http_health_check" "fancy_fe_hc" {
-  name = "fancy-fe-hc"
-  port = "8080"
-  request_path = "/"
-  check_interval_sec = 30
-  timeout_sec = 10
-  healthy_threshold = 1
-  unhealthy_threshold = 3
-}
-
-resource "google_compute_http_health_check" "fancy_be_hc" {
-  name = "fancy-be-hc"
-  port = "8081"
-  request_path = "/api/orders"
-  check_interval_sec = 30
-  timeout_sec = 10
-  healthy_threshold = 1
-  unhealthy_threshold = 3
-}
-
-
-resource "google_compute_http_health_check" "fancy_fe_frontend_hc" {
-  name = "fancy-fe-frontend-hc"
-  request_path = "/"
-  port = 8080
-}
-
-resource "google_compute_http_health_check" "fancy_be_orders_hc" {
-  name = "fancy-be-orders-hc"
-  request_path = "/api/orders"
-  port = 8081
-}
-
-resource "google_compute_http_health_check" "fancy_be_products_hc" {
-  name = "fancy-be-products-hc"
-  request_path = "/api/products"
-  port = 8082
-}
 
 ## Firewall Rules to allow Front-End and Back-End
 
