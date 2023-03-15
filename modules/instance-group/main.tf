66221a419c2e4f3c39dcb5fa86498051dc91680a
gregory hendrickson <greg@hndrx.co>
2023-03-15 15:54:02 -0700

    consolidate file

--- modules/instance-group/main.tf
+++ modules/instance-group/main.tf
@@ -59,3 +59,28 @@ resource "google_compute_instance_group_manager" "fancy_be_mig" {
     google_compute_backend_service.fancy_backend_service
   ]
 }
+
+
+
+resource "google_compute_region_autoscaler" "fancy_fe_autoscaler" {
+  name = "fancy-fe-autoscaler"
+  target = google_compute_instance_group_manager.fancy_fe_mig.self_link
+  cooldown_period_sec = 60
+  load_balancing_utilization_target = 0.6
+  max_replicas = 2
+
+  depends_on = [
+    module.google_compute_instance_group_manager.fancy_fe_mig
+  ]
+}
+
+resource "google_compute_region_autoscaler" "fancy_be_autoscaler" {
+  name = "fancy-be-autoscaler"
+  target = google_compute_instance_group_manager.fancy_be_mig.self_link
+  cooldown_period_sec = 60
+  load_balancing_utilization_target = 0.6
+  max_replicas = 2
+  depends_on = [
+    google_compute_instance_group_manager.fancy_be_mig
+  ]
+}
