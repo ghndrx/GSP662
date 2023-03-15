d345ee1d49a7ed858406041b8d6449b10c7cfc54
gregory hendrickson <greg@hndrx.co>
2023-03-15 13:45:39 -0700

    .

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "project_id" {
   type        = string
   description = "The ID of the project to deploy resources into"
 }
+
+variable "region" {
+  type        = string
+  description = "The region to deploy resources into"
+}
+
+variable "zone" {
+  type        = string
+  description = "The zone in which resource to be deployed"
+}
