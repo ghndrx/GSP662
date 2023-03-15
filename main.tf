d3ec98e7316d2ec4601fee2a8f82ac3f5c0a3f50
gregory hendrickson <greg@hndrx.co>
2023-03-15 12:26:19 -0700

    .

--- main.tf
+++ main.tf
@@ -1,40 +1,46 @@
-terraform {
-  required_version = ">= 0.14.0"
-}
 
-module "network" {
-  source      = "./modules/network"
-  project_id  = var.project_id
-  region      = var.region
-  network_cidr= var.network_cidr
-}
-
+# Module Configuration
 module "backend" {
   source = "./modules/backend"
-  sa_email = "example@project-id.iam.gserviceaccount.com"
-  image_name = "backend-image"
-  project_id = var.project_id
-  region = var.region
-}
 
-
-module "nat_gateway" {
-  source = "./modules/network/nat_gateway"
-  network_name = module.network.network_name
-  region = var.region
-}
-
-module "firewall" {
-  source = "./modules/firewall"
-  network_name = module.network.network_name
+  instance_name   = var.backend_instance_name
+  machine_type    = var.backend_machine_type
+  zone            = var.zone
+  network_name    = module.network.network_name
+  subnet_name     = module.network.backend_subnet_name
+  tags            = ["backend"]
+  image_family    = "debian-11"
+  image_project   = "debian-cloud"
 }
 
 module "frontend" {
   source = "./modules/frontend"
-  backend_service_name = module.backend.backend_service_name
-  network_name = module.network.network_name
+
+  instance_name   = var.frontend_instance_name
+  machine_type    = var.frontend_machine_type
+  zone            = var.zone
+  network_name    = module.network.network_name
+  subnet_name     = module.network.frontend_subnet_name
+  tags            = ["frontend"]
 }
 
-output "frontend_external_ip" {
-  value = module.frontend.external_ip
+module "network" {
+  source = "./modules/network"
+
+  project_id        = var.project_id
+  region            = var.region
+  network_name      = var.network_name
+  subnet_cidrs      = var.subnet_cidrs
+  nat_gateway_count = var.nat_gateway_count
+  firewall_name     = var.firewall_name
+}
+
+module "firewall" {
+  source = "./modules/firewall"
+
+  project_id     = var.project_id
+  network_name   = module.network.network_name
+  firewall_name  = var.firewall_name
+  allowed_ports  = var.allowed_ports
+  target_tags    = ["backend"]
 }
