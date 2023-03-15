979b0b0eaaacec0b592852b815f764ac4cf62eec
gregory hendrickson <greg@hndrx.co>
2023-03-15 16:03:34 -0700

    update network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -201,24 +201,4 @@ resource "google_compute_http_health_check" "fancy_be_products_hc" {
   port = 8082
 }
 
-## Firewall Rules to allow Front-End and Back-End
 
-resource "google_compute_firewall" "fw_fe" {
-  name    = "fw-fe"
-  network = "default"
-  allow {
-    protocol = "tcp"
-    ports    = ["8080"]
-  }
-  target_tags = ["frontend"]
-}
-
-resource "google_compute_firewall" "fw_be" {
-  name    = "fw-be"
-  network = "default"
-  allow {
-    protocol = "tcp"
-    ports    = ["8081-8082"]
-  }
-  target_tags = ["backend"]
-}
