a3de3c2515ec7a8576db477184f54029b1a84e0f
gregory hendrickson <greg@hndrx.co>
2023-03-15 12:24:26 -0700

    update variables

--- variables.tf
+++ variables.tf
@@ -12,3 +12,27 @@ variable "network_cidr" {
   type        = string
   description = "The CIDR block for the VPC network"
 }
+
+
+variable "network_name" {}
+
+variable "subnet_name" {}
+
+variable "firewall_name" {}
+
+module "network" {
+  source  = "./modules/network"
+  project_id = var.project_id
+  region = var.region
+  network_name = var.network_name
+  subnet_name = var.subnet_name
+}
+
+module "firewall" {
+  source  = "./modules/firewall"
+  project_id = var.project_id
+  region = var.region
+  network_name = module.network.network_name
+  subnet_name = module.network.subnet_name
+  firewall_name = var.firewall_name
+}
