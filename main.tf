019bd52d14538083e5e4e3b2f410f81b782b4a44
gregory hendrickson <greg@hndrx.co>
2023-03-15 13:34:23 -0700

    .

--- main.tf
+++ main.tf
@@ -6,33 +6,7 @@ module "instances" {
 
 }
 
-module "instance-group" {
-  source = "./modules/instance-group"
-}
-
-
-module "network" {
-  source = "./modules/network"
-
-}
 
 module "storage" {
   source = "./modules/storage"
-  project_id = var.project_id
-}
-
-module "autoscale" {
-  source = "./modules/autoscale"
-}
-
-module "firewall" {
-  source = "./modules/network/firewall"
-}
-
-module "healthchecks" {
-  source = "./modules/network/healthchecks"  
-}
-
-module "loadbalancer" {
-  source = "./modules/network/loadbalancer"
 }
